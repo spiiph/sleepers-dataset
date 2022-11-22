a7f4f4bc6a1a53ba36289fed5f36232e6bb61e5a
Henrik Leijon <henrik.leijon@trafikverket.se>
2022-11-22 18:02:05 +0100

    Add class to represent binary masks
    Add the mask module and Mask class to represent binary masks, and
    enable conversion from masks to contours and polygons.
    
    Rewrite the console module to use a single entrypoint for all available
    commands.

--- dataset/console.py
+++ dataset/console.py
@@ -2,9 +2,10 @@ import argparse
 import logging
 import os
 
+from dataclasses import dataclass, field
 from os.path import join
 
-from dataset import patches
+from dataset import patches, mask
 
 
 LOG_LEVELS = list(logging._nameToLevel.keys())[:-1] # pylint: disable=protected-access
@@ -20,7 +21,7 @@ def setup_logger(name: str, level: str = "INFO") -> logging.Logger:
     handler = logging.StreamHandler()
     handler.setLevel(level)
 
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s")
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
@@ -28,41 +29,25 @@ def setup_logger(name: str, level: str = "INFO") -> logging.Logger:
     return logger
 
 
-def hello_world() -> None:
+def hello_world(args: argparse.Namespace) -> None:
     """
     Hello World entry point for testing
     """
-    print("Hello world!")
+
+    logger = setup_logger("dataset", level=args.log_level)
+    logger.debug("Hello World!")
+    logger.info("Hello World!")
+    logger.warning("Hello World!")
+    logger.error("Hello World!")
+    logger.fatal("Hello World!")
 
 
-def create_patches() -> None:
+def create_patches(args: argparse.Namespace) -> None:
     """
     Create patches from one image and one mask
     """
-    parser = argparse.ArgumentParser(description="Test patches")
-    parser.add_argument(
-        "image",
-        help="Image file"
-    )
-    parser.add_argument(
-        "mask",
-        help="Mask file"
-    )
-    parser.add_argument(
-        "dest",
-        help="Destination dir"
-    )
-    parser.add_argument(
-        "--log-level", "-l",
-        type=str,
-        choices=LOG_LEVELS,
-        default="INFO"
-    )
-
-    args = parser.parse_args()
-
     logger = setup_logger("dataset", level=args.log_level)
-    logger.info(f"Running {__name__} with the following parameters:")
+    logger.info("Running create-patches with the following parameters:")
     logger.info(f"  image: {args.image}")
     logger.info(f"  mask: {args.mask}")
     logger.info(f"  dest: {args.dest}")
@@ -72,5 +57,98 @@ def create_patches() -> None:
             logger.debug(f"Creating missing directory {join(args.dest, subdir)}")
             os.makedirs(join(args.dest, subdir))
 
-    patch = patches.Patches(args.image, args.mask)
-    patch.write_patches(args.dest, splits=8, store_empty=False)
+    patches_obj = patches.Patches(args.image, args.mask)
+    patches_obj.write_patches(args.dest, splits=8, store_empty=False)
+
+
+def test_polygons(args: argparse.Namespace) -> None:
+    """
+    Test the conversion of masks to contours and polygons
+    """
+    logger = setup_logger("dataset", level=args.log_level)
+    logger.info("Running test-contours-and-polygons with the following parameters:")
+    logger.info(f"  mask: {args.mask}")
+
+    mask_obj = mask.Mask(args.mask)
+    logger.info("Contours (not normalized):")
+    for contour in mask_obj.to_contours(normalize=False):
+        logger.info(f">>> {contour.T[0].tolist()}")
+        logger.info(f">>> {contour.T[1].tolist()}")
+    logger.info("Contours (normalized):")
+    for contour in mask_obj.to_contours(normalize=True):
+        logger.info(f">>> {contour.T[0].tolist()}")
+        logger.info(f">>> {contour.T[1].tolist()}")
+    logger.info("Polygons:")
+    for polygon in mask_obj.to_polygons(normalize=True):
+        logger.info(f">>> {polygon}")
+
+
+@dataclass
+class Argument:
+    """
+    Class to define reusable arguments for ArgumentParser
+    """
+    args: list = field()
+    kwargs: dict = field()
+
+
+def main() -> None:
+    """
+    Entry point for all sub-commands
+    """
+    parser = argparse.ArgumentParser(description="Run sleeper-dataset commands")
+    subparsers = parser.add_subparsers(help="Command to run")
+
+    log_level = Argument(
+        args=["--log-level", "-l"],
+        kwargs= {
+            "type": str,
+            "choices": LOG_LEVELS,
+            "default": "INFO"
+        }
+    )
+
+    # Set up the hello parser
+    hello_parser = subparsers.add_parser(
+        "hello",
+        help="Run the hello sub-command"
+    )
+    hello_parser.set_defaults(func=hello_world)
+    hello_parser.add_argument(*log_level.args, **log_level.kwargs)
+
+    # Setup the create-patches parser
+    patches_parser = subparsers.add_parser(
+        "create-patches",
+        help="Run the create-patches sub-command"
+    )
+    patches_parser.add_argument(
+        "image",
+        help="Image file"
+    )
+    patches_parser.add_argument(
+        "mask",
+        help="Mask file"
+    )
+    patches_parser.add_argument(
+        "dest",
+        help="Destination dir"
+    )
+    patches_parser.set_defaults(func=create_patches)
+    patches_parser.add_argument(*log_level.args, **log_level.kwargs)
+
+    # Set up the test-polygons parser
+    polygons_parser = subparsers.add_parser(
+        "test-polygons",
+        help="Run the teset-polygons sub-command"
+    )
+    polygons_parser.add_argument(
+        "mask",
+        help="Mask file"
+    )
+    polygons_parser.set_defaults(func=test_polygons)
+    polygons_parser.add_argument(*log_level.args, **log_level.kwargs)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    args.func(args)
---
+++ dataset/mask.py
@@ -0,0 +1,80 @@
+import logging
+
+import cv2 # type: ignore
+import numpy as np
+
+from PIL import Image
+
+logger = logging.getLogger("dataset.mask")
+
+
+class Mask:
+    """
+    Class to represent a segmentation annotation as a binary mask
+
+    NOTE: This class can only handle images with two colours, in palette mode.
+    """
+
+    def __init__(self, mask_path: str):
+        """
+        Initialize a Mask object
+
+        :param mask_path: Path to mask
+        """
+        logger.debug(f"Creating Mask object with mask_path = {mask_path}")
+
+        self.mask_path: str = mask_path
+        self._data: np.ndarray | None = None
+        self._contours: list[np.ndarray] | None = None
+        self._norm: np.ndarray
+
+
+    def _load_data(self) -> np.ndarray:
+        """
+        Load the image data for the mask and construct the image norm
+
+        Load the image data for the mask as a NumPy array. Construct the image norm out of the
+        image width and height.
+
+        :return: The image data of the mask as a
+        """
+        if self._data is None:
+            self._data = np.asarray(Image.open(self.mask_path))
+            self._norm = np.array([self._data.shape[1], self._data.shape[0]])
+
+        return self._data
+
+
+    def to_contours(self, normalize: bool = True) -> list[np.ndarray]:
+        """
+        Convert the masks to contours
+
+        :param normalize: Normalize the contour to the width and height of the image
+        :return: List of NumPy arrays, one for each identified contour
+        """
+        self._load_data()
+
+        if self._contours is None:
+            contours, _ = cv2.findContours(self._data, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1)
+            self._contours = [
+                contour.reshape(contour.shape[0], contour.shape[2])
+                for contour in contours
+            ]
+
+        if normalize:
+            return [contour / self._norm for contour in self._contours]
+
+        return self._contours
+
+
+    def to_polygons(self, normalize: bool = True) -> list[list[int | float]]:
+        """
+        Convert the contours to YOLO polygons
+
+        :param normalize: Normalize the polygons to the width and height of the image
+        :return: List of flat lists of the format [class, x1, y1, x2, y2, ...]
+        """
+        return [
+            [0] + contour.flatten().tolist()
+            for contour in self.to_contours(normalize=normalize)
+        ]
